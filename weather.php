fd053993afd6f5fcc64d707023ccf66f7f90fb5b
Lee Garner <lee@leegarner.com>
2022-07-31 17:20:13 -0700

    Convert location strings to coordinates

--- weather.php
+++ weather.php
@@ -11,25 +11,8 @@
  * @filesource
  */
 
-/**
- * Global array of table names from glFusion.
- * @global array $_TABLES
- */
-global $_TABLES;
-
-/**
- * Global table name prefix.
- * @global string $_DB_table_prefix
- */
-global $_DB_table_prefix;
-
-$_table_prefix = $_DB_table_prefix . 'weather';
-
-// Deprecated cache table, delete when only glFusion cache is supported
-$_TABLES['weather_cache']       = $_table_prefix . '_cache';
-
 $_CONF_WEATHER['pi_name'] = 'weather';
 $_CONF_WEATHER['pi_version'] = '2.0.2';
-$_CONF_WEATHER['gl_version'] = '1.7.8';
+$_CONF_WEATHER['gl_version'] = '2.0.0';
 $_CONF_WEATHER['pi_url'] = 'http://www.leegarner.com';
 $_CONF_WEATHER['pi_display_name'] = 'Weather';
